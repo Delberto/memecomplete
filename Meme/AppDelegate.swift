166803fd149991ae57ba5434e489430a13cec6db
Delberto Martínez <delbertoms@gmail.com>
2026-04-01 18:00:58 -0600

    Swift 6 fixes
    Code changes to support swift6

--- Meme/AppDelegate.swift
+++ Meme/AppDelegate.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var memes = [Meme]()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
